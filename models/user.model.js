c6a889ec8b22d0f0a26691ee39858ebebb6ef5ca
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-01-25 21:22:17 +0000

    feat: implement user model with create and retrieve user functionality

---
+++ models/user.model.js
@@ -0,0 +1,19 @@
+const { db } = require("../config/firebase.config");
+const { COLLECTIONS } = require("../config/constants");
+
+class UserModel {
+  static async createUser(uid, userData) {
+    const userRef = db.collection(COLLECTIONS.USERS).doc(uid);
+    await userRef.set({
+      ...userData,
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+    });
+    return userRef;
+  }
+
+  static async getUserById(uid) {
+    const userDoc = await db.collection(COLLECTIONS.USERS).doc(uid).get();
+    return userDoc.exists ? { id: userDoc.id, ...userDoc.data() } : null;
+  }
+}
