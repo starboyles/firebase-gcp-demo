d316d2e51062134a90e8d6b7f12142c309ab5bd0
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-01-25 17:10:38 +0000

    feat: add Firebase configuration for Firestore integration

---
+++ config/firebase.config.js
@@ -0,0 +1,10 @@
+const admin = require("firebase-admin")
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    // databaseURL: process.env.FIREBASE_DATABASE_URL
+})
+
+const admin = admin.firestore()
+module.exports = { admin, db };
