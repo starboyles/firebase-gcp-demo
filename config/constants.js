3813d732e82a827278fe0e7eaecf7a6f2bbc2ea6
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-01-25 17:17:31 +0000

    feat: add application constants for collections, roles, task status, and priority

---
+++ config/constants.js
@@ -0,0 +1,24 @@
+//Appplication constants
+
+module.exports = {
+  COLLECTIONS: {
+    USERS: "users",
+    TEAMS: "teams",
+    TASKS: "tasks",
+  },
+  ROLES: {
+    ADMIN: "admin",
+    USER: "user",
+  },
+  TASK_STATUS: {
+    TODO: "todo",
+    in_progress: "in-progress",
+    REVIEW: "review",
+    DONE: "done",
+  },
+  TASK_PRIORITY: {
+    LOW: "low",
+    MEDIUM: "medium",
+    HIGH: "high",
+  }
+};
