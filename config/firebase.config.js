0927d3646c4818e968020f9ed3ac12d8240ab72f
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-01-26 22:37:27 +0000

    fix: update Firebase configuration to use local service account file

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+
+const serviceAccount = require("../firebase-service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   // databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
-const admin = admin.firestore();
+const db = admin.firestore();
 module.exports = { admin, db };
