dd0ac7b577c6bce30fbdf3c5b6589e81dc8320b3
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-01-26 23:42:39 +0000

    fix: typo

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("../firebase-service-account.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
   // databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
